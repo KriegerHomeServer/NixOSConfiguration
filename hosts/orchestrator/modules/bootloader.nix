13c6e998ba0954a3b07aeff9554d0f5714e30136
Teegan Krieger <teegankrieger@gmail.com>
2024-09-15 15:06:25 -0400

    boot loader

--- hosts/orchestrator/modules/bootloader.nix
+++ hosts/orchestrator/modules/bootloader.nix
@@ -12,15 +12,25 @@
   # Define the packages configuration
   config = {
 
+    # Description: Define options for the bootloader
+    # Type: Attribute Set
     boot.loader = {
 
-      # Enable GRUB as the bootloader
+      # Description: Define options for the GRUB bootloader
+      # Type: Attribute Set
       grub = {
 
+        # Description: Whether to enable the GNU GRUB boot loader
+        # Type: Boolean
         enable = true;
+
+        # Description: If set to true, append entries for other OSs detected by os-prober.
+        # Type: Boolean
         useOSProber = true;
+
+        # Description: The device on which the GRUB boot loader will be installed
+        # Type: String
         device = "/dev/sda";
-        efiSupport = true;
 
       };
 
